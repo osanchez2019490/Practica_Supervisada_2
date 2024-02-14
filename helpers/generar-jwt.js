d6dcd4a24678f0a54271277ef8661265f0320295
osanchez-2019490 <osanchez-2019490@kinal.edu.gt>
2024-02-14 13:06:06 -0600

    Logica de generar

---
+++ helpers/generar-jwt.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const generarJWT = (uid = '') => {
+    return new Promise((resolve, reject) =>{
+        const payload = { _id};
+        jwt.sign(
+            payload,
+            process.env.SECRETORPRIVATEKEY,
+            {
+                expiresIn: '2h',
+            },
+            (err, token) =>{
+                err ? (console.log(err), reject('Nose pudo generar el token')): resolve(token);
+            }
+        );
+    });
+};
+
+module.exports ={
+    generarJWT
+}
