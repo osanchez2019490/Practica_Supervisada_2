7698afad0e6d440dda0e39fbd089003509883fb6
osanchez-2019490 <informatica@alumno.local>
2024-02-27 13:22:08 -0600

    Se agrega los roles preterminados esperando aprovacion del profesor

--- models/role.js
+++ models/role.js
@@ -1,5 +1,10 @@
 const { Schema, model} = require('mongoose');
 
+const defaultRoles = [
+    { role: 'TEACHER_ROLE'},
+    { role: 'STUDENTE_ROLE'}
+];
+
 const RoleSchema = Schema({
     role: {
         type: String,
@@ -7,4 +12,18 @@ const RoleSchema = Schema({
     }
 });
 
-module.exports = model ('Role', RoleSchema);
+const Role = model('Role', RoleSchema);
+
+defaultRoles.forEach(async (defaultRole) => {
+    try {
+        const existeRole = await Role.findOne({ role: defaultRole.role });
+        if(!existeRole) {
+            await Role.create(defaultRole);
+            console.log(`Rol preterminado ${defaultRole.role} creado`)
+        }
+    } catch (e) {
+        console.error('Error al crear los roles preterminados: ', e)
+    }
+});
+
+module.exports = Role;
